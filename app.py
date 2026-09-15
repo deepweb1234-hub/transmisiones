058ddce05798f5aa2bbc551e47caf6249ba837c1
deepweb1234-hub <marcosgi674@gmail.com>
2026-09-14 20:49:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -76,12 +76,13 @@ def start_streaming():
                 "-stream_loop", "-1",
 
                 # -----------------------------------------------
-                # PARÁMETROS DE RECONEXIÓN SEGURA (EVITA BANEOS)
+                # PARÁMETROS DE RECONEXIÓN CORREGIDOS
                 # -----------------------------------------------
                 "-reconnect", "1",
                 "-reconnect_at_eof", "1",
                 "-reconnect_streamed", "1",
-                "-reconnect_delay_max", "3",
+                "-reconnect_delay_max", "5",
+                "-listen_timeout", "5",
                 
 
                 # -----------------------------------------------
